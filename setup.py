63de274606b241666b39eff7a8c21a0b3c6f6be6
npdep <183926505+npdep@users.noreply.github.com>
2024-11-03 20:52:30 +0100

    license info changed

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
     description="Receiver Modules for Network Protocol Data Exfiltration Project Server",
     classifiers=[
         "Operating System :: OS Independent ",
-        "Programming Language :: Python :: 3 ",
-        "License :: OSI Approved :: MIT License "
+        "Programming Language :: Python :: 3 "
     ],
     long_description=desc,
     long_description_content_type="text/markdown",
